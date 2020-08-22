f3f715ea823fb69ea53f9e14380a7f9e534459d7
ilzfor <66512876+ilzfor@users.noreply.github.com>
2020-08-22 20:44:04 +0800

    移動往面相方向

---
+++ Unity 3DGAME/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("目標")]
+    public Transform target;
+    [Header("速度"), Range(0, 100)]
+    public float speed = 1;
+    [Header("旋轉速度"), Range(0, 100)]
+    public float turn = 1;
+    [Header("上下角度限制")]
+    public Vector2 limit = new Vector2(-30, 30);
+    /// <summary>
+    /// 滑鼠控制旋轉角度
+    /// </summary>
+    private Quaternion rot;
+   /// <summary>
+   /// 追蹤
+   /// </summary>
+    private void Track()
+    {
+        Vector3 posA = transform.position;                               //A點
+        Vector3 posB = target.position;                                  //B點
+        posA = Vector3.Lerp(posA, posB, Time.deltaTime * speed);         //A點=Vector3.插值(A點B點,Time.deltaTime*speed);
+        transform.position = posA;                                     //攝影機.座標=A點
+        ///旋轉
+        rot.x += -Input.GetAxis("Mouse Y") * turn;
+        rot.y += Input.GetAxis("Mouse X") * turn;
+        rot.x = Mathf.Clamp(rot.x, limit.x, limit.y);
+        transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);
+
+    }
+    private void Awake()
+    {
+        Cursor.visible = false;//指標.可視=否
+    }
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
--- Unity 3DGAME/Assets/Scripts/Player.cs
+++ Unity 3DGAME/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     #region 欄位
     [Header("速度"), Range(0, 1000)]
     public float speed = 1;
+    [Header("旋轉角度"), Range(0, 1000)]
+    public float turn = 60;
 
     private float attack = 10;
     private float hp = 100;
@@ -15,6 +17,7 @@ public class Player : MonoBehaviour
 
     private Rigidbody rig;
     private Animator ani;
+    private Transform cam;//攝影機根物件
     #endregion
     #region 事件
     private void Awake()
@@ -23,6 +26,7 @@ public class Player : MonoBehaviour
         //泛型:所有類型
         rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
+        cam=GameObject.Find("攝影機根物件").transform;
     }
     private void FixedUpdate()
     {
@@ -37,9 +41,16 @@ public class Player : MonoBehaviour
     {
         float v = Input.GetAxis("Vertical");//前後:WS 上下
         float h = Input.GetAxis("Horizontal");
-        Vector3 pos =transform.forward*v+transform.right*h ;//移動座標(左右，0，前後)
+        Vector3 pos =cam.forward*v+cam.right*h ;//移動座標=攝影機前後左右(左右，0，前後)
         rig.MovePosition(transform.position + pos*speed);//移動座標(原本座標+移動座標*速度)
 
+        ani.SetFloat("移動", Mathf.Abs(v) + Mathf.Abs(h));//設定浮點數(絕對值V與H)
+        if (v != 0 || h != 0)//如果控制中
+        {
+            pos.y = 0;
+            Quaternion angle = Quaternion.LookRotation(pos);//B角度=面向(移動座標)
+            transform.rotation = Quaternion.Slerp(transform.rotation, angle, turn);//A角度.插值(A角度，B角度，旋轉角度)
+        }
     }
     private void Attack()
     {
